6a9076bbbf7e5127b972c0cf213e51f393aed4d9
SalmanFarrisi <muhammad.farrisi@mhs.unsoed.ac.id>
2023-11-13 12:18:55 +0700

    belajar ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'mahasiswa',
     pathMatch: 'full'
+  },
+  {
+    path: 'mahasiswa',
+    loadChildren: () => import('./mahasiswa/mahasiswa.module').then(m => m.MahasiswaPageModule)
   },
 ];
 
